a568c44fe0c7659e090501f9567d2f6321cf2aa7
Jared Heinrichs <jared.heinrichs@gmail.com>
2025-04-09 10:21:57 -0500

    Create pull_latest_git.sh

---
+++ pull_latest_git.sh
@@ -0,0 +1,7 @@
+#Pull latest servarr from git
+#==========================================================
+# Make sure to run this command before running this file 
+# chmod +x pull_latest_git.sh
+#==========================================================
+git pull https://github.com/jheinrichs79/servarr.git
+#END
