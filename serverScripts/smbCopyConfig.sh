9d747d3d9dd7372cb1828370c974695df10459b7
Jared Heinrichs <jared.heinrichs@gmail.com>
2025-05-14 07:52:43 -0500

    Create smbCopyConfig.sh

---
+++ serverScripts/smbCopyConfig.sh
@@ -0,0 +1,4 @@
+#Copy SMB config file and verify smb.conf
+#========================================
+sudo cp /home/homeservarr/git/homeServarr/configFiles/full.smb.conf /etc/samba/smb.conf
+sudo nano /etc/samba/smb.conf
